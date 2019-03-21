cfb97291c4ce6fb9ed725307dcec4d280b08c81d
JoanBarduena <36154195+JoanBarduena@users.noreply.github.com>
2019-03-21 17:17:57 +0100

    Base code cleaned and STL implemeted

---
+++ CutSceneManager/Motor2D/j1Player.cpp
@@ -0,0 +1,63 @@
+#include "j1Player.h"
+#include "j1App.h"
+#include "j1Textures.h"
+#include "j1Render.h"
+#include "j1Input.h"
+#include "p2Log.h"
+
+j1Player::j1Player(): j1Module()
+{
+	name.assign("player");
+}
+
+j1Player::~j1Player()
+{
+}
+
+bool j1Player::Awake()
+{
+	
+
+	return true;
+}
+
+bool j1Player::Start()
+{
+
+	graphics = App->tex->Load("textures/character.png");
+
+	position.x = 0;
+	position.y = 600;
+	
+	return true;
+}
+
+bool j1Player::PreUpdate()
+{
+
+
+
+	return true;
+}
+
+
+bool j1Player::Update(float dt)
+{
+	return true;
+}
+
+bool j1Player::PostUpdate()
+{
+
+	App->render->Blit(graphics, position.x, position.y);
+
+	return true;
+}
+
+bool j1Player::CleanUp()
+{
+
+	App->tex->UnLoad(graphics);
+
+	return true;
+}
